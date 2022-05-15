c3b6de64d53cedebc7da5e19f919dd0771c9b954
Osita Iyadi <oiyadi@appzonegroup.com>
2022-05-16 00:20:31 +0100

    Create tables using creat_all()

--- flash-hello-app.py
+++ flash-hello-app.py
@@ -4,8 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://oiyadi:oyinlola@localhost:5432/oiyadi'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+class Person(db.Model):
+    __tablename__ = 'persons'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+
+db.create_all()
 
 @app.route('/')
 def index():
