b8bf279406b9e7dced7f905712782b1b1013e43c
Osita Iyadi <oiyadi@appzonegroup.com>
2022-05-15 23:12:09 +0100

    Add sqalchemy variable

--- flash-hello-app.py
+++ flash-hello-app.py
@@ -1,6 +1,11 @@
+from re import S
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://oiyadi:oyinlola@localhost:5432/oiyadi'
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
