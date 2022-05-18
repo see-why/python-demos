e21281a24d75acba5f30300350fb0da041fc22ca
see-why <iyadicy@gmail.com>
2022-05-18 04:12:49 +0100

    Complete app.py script

--- app.py
+++ app.py
@@ -1,12 +1,51 @@
-from flask import Flask
+import sys
+from flask import Flask, abort, jsonify, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:abc@localhost:5432/todoapp'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Todo(db.Model):
+  __tablename__ = 'todos'
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String(), nullable=False)
+  completed = db.Column(db.Boolean, nullable=False, default=False)
+
+  def __repr__(self):
+    return f'<Todo {self.id} {self.description}>'
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():   
+   body={}
+   error = False
+   try: 
+       description =  request.get_json()['description']
+       todo = Todo(description=description)
+       body['description'] = todo.description
+       db.session.add(todo)
+       db.session.commit()
+   except:        
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+   finally:
+        db.session.close()           
+        if  error == True:
+            abort(400)
+        else:            
+            return jsonify(body)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html', data=Todo.query.all())
+
+
 
 
 #always include this at the bottom of your code
 if __name__ == '__main__':
-   app.run(host="0.0.0.0")
+   app.run(host="0.0.0.0", port=3000)
