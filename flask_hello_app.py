3d4f659998e3a8081cd5651c2956b75bb6160114
see-why <iyadicy@gmail.com>
2022-05-18 02:59:28 +0100

    Add to-do class

--- flask_hello_app.py
+++ flask_hello_app.py
@@ -18,6 +18,14 @@ class Person(db.Model):
     def __repr__(self):
         return f'<Person ID:{self.id} NAME:{self.name} >'
 
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Person ID:{self.id} DESCRIPTION:{self.description} >'
+
 # db.create_all()
 
 @app.route('/')
